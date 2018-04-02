848ebbbc80d242266fe74c7fbca5e7a7570c0731
roboman2444 <roboman2444@gmail.com>
2018-04-02 15:36:16 -0400

    more test

--- test.py
+++ test.py
@@ -5,7 +5,24 @@ def generate_patch_location(image_shape, stride, kernel_size):
 	width= image_shape[0]
 	height= image_shape[1]
 	X,Y = np.mgrid[-x_rem:width-x_rem:stride, -y_rem:height:stride]
-	xy =np.vstack((X.flatten(), Y.flatten())).T
-	return xy
+
+	Xsort = sorted(set(X.flatten()))
+	Ysort = sorted(set(Y.flatten()))
+
+	Xdelt = []
+	Ydelt = []
+	for za, zb in zip(Xsort[1:], Xsort):
+		Xdelt.append(za-zb)
+	for za, zb in zip(Ysort[1:], Ysort):
+		Ydelt.append(za-zb)
+
+	print(Xsort)
+	print(Xdelt)
+	print(Ysort)
+	print(Ydelt)
+
+
+#	xy =np.vstack((X.flatten(), Y.flatten())).T
+#	return xy
 stackattack = generate_patch_location([424, 512], 16, 48)
-print(stackattack)
+#prettyprint(stackattack)
